bdf4e465dea32fad3e297dc9040d50e09f00b90f
Breolson <NichkovME@yandex.ru>
2019-01-26 14:56:54 +0300

    Vanya at main.c str18

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: gkshleri <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/26 13:26:27 by gkshleri          #+#    #+#             */
-/*   Updated: 2019/01/26 13:34:00 by gkshleri         ###   ########.fr       */
+/*   Updated: 2019/01/26 14:56:15 by breolson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,6 @@
 int		main(void)
 {
 	char a[5] = "Hello";
-	ft_printf("%s World!", a);
+	ft_printf("%s Vanya World!", a);
 	return (0);
 }
