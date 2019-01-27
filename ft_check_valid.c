8224ff04c8be7dbe61269cf4f0b893d0a40ced37
IvanLevin <levvanya@gmail.com>
2019-01-27 18:25:48 +0300

    Создал проверки, добавил либу

--- ft_check_valid.c
+++ ft_check_valid.c
@@ -1,36 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_parsing.c                                       :+:      :+:    :+:   */
+/*   ft_check_valid.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gkshleri <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/01/26 13:19:30 by gkshleri          #+#    #+#             */
-/*   Updated: 2019/01/27 16:16:02 by gkshleri         ###   ########.fr       */
+/*   Created: 2019/01/27 16:03:03 by gkshleri          #+#    #+#             */
+/*   Updated: 2019/01/27 16:31:56 by gkshleri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int		parsing(char *argv, va_list ap)
+char	*ft_flag(char **argv)
 {
-	int		i;
+	t_list	*list;
 
-	i = 0;
-	if (argv[i] == '-' || argv[i] == '+' || argv[i] == ' ' || \
-			argv[i] == '#' || argv[i] == '0')
-	{
-		argv = ft_flag(argv);
-	}
-	if (argv[i] == '*' || (argv[i] >= '0' && argv[i] <= '9'))
-	{
-		argv = ft_width(argv, ap);
+}
 
-	}
-	if (argv[i] == '.' && (argv[i + 1] == '*' || \
-				(argv[i + 1] >= '0' && argv[i + 1] <= '9')))
-	{
-		argv = ft_accuracy(argv, ap);
-	}
-	return (1);
+char	*ft_width(char **argv, va_list ap)
+{
+
+}
+
+char	*ft_accuracy(char **argv, va_list ap)
+{
+
 }
--- ft_printf.h
+++ ft_printf.h
@@ -6,19 +6,30 @@
 /*   By: gkshleri <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/26 13:02:44 by gkshleri          #+#    #+#             */
-/*   Updated: 2019/01/26 13:33:56 by gkshleri         ###   ########.fr       */
+/*   Updated: 2019/01/27 17:19:31 by gkshleri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
+#include "libft/includes/libft.h"
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdarg.h>
 
+define struct	t_lists
+{
+	char		*arr;
+	char		*flag;
+	int 		width;
+}				s_lists
+
 int		ft_printf(const char *argv, ...);
 int		parsing(char *argv, va_list ap);
+char	*ft_flag(char **argv);
+char	*ft_width(char **argv, va_list ap);
+char	*ft_accuracy(char **argv, va_list ap);
 
 #endif
