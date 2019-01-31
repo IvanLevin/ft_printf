f8624498d7652bdcaa09724e73dca68b062508b5
IvanLevin <levvanya@gmail.com>
2019-01-31 14:17:44 +0300

    Изменил тип "с"

--- print_free.c
+++ print_free.c
@@ -6,7 +6,7 @@
 /*   By: breolson <breolson@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/30 05:32:58 by breolson          #+#    #+#             */
-/*   Updated: 2019/01/30 17:28:39 by gkshleri         ###   ########.fr       */
+/*   Updated: 2019/01/31 14:15:29 by gkshleri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,18 @@
 
 // печатает строку, фришит память + запоминает сколько байт вывел
 
-void		ft_print_free(char **str, t_lists *list, int max)
+void		ft_print_free(char *str, t_lists *list, int max)
 {
-	char	*tmp;
 	int 	i;
 
 	i = 0;
-	if (!str || !*str)
+	if (!str)
 		return ;
-	tmp = *str;
 	while (max)
 	{
-		write(1, tmp++, 1);
+		write(1, str++, 1);
 		max--;
 		i++;
 	}
-	free(*str);
-	*str = NULL;
 	list->len += i;
 }
