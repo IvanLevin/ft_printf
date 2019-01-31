a158b99e3dae79fd8d8d285a306b706bfcc87cb0
Breolson <NichkovME@yandex.ru>
2019-01-31 15:32:30 +0300

    merge w master

--- main.c
+++ main.c
@@ -15,18 +15,18 @@
 
 int		main(void)
 {
-	char a[5] = "Hello";
+//	char a[5] = "Hello";
 	// int  d = 232;
 
 //	ft_printf("%#0--+*.* TEST_WORD \n", 32, 12);
 //
 //	printf("\n-----------------------\n");
-	printf("ZDOROVYI printf    \n");
-	printf("%10.3s", a);
+//	printf("ZDOROVYI printf    \n");
+//	printf("%10.3s", a);
 //	printf("\n-----------------------\n");
 //	printf("\n-----------------------\n");
-	printf("printf KURIL'SCHIKA  \n");
-		ft_printf("%10.3s", a);
+//	printf("printf KURIL'SCHIKA  \n");
+//		ft_printf("%10.3s", a);
 //	printf("\n-----------------------\n");
 //	printf("\n***********************\n");
 //	//printf("\n%d", ft_printf("%10s", a));
@@ -36,7 +36,7 @@ int		main(void)
 //	printf("\n-----------------------\n");
 //	printf("\n-----------------------\n");
 //	printf("printf KURIL'SCHIKA  \n");
-		// ft_printf("%-17c", 'A');
+		 ft_printf("%-17c", 'A');
 		// printf("\n-----------------------\n");
 		// ft_printf("%04d", d);
 //	printf("\n-----------------------\n");
