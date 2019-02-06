216ed33ac6c08dffae2037bc7246dac22f3b9995
IvanLevin <levvanya@gmail.com>
2019-02-06 16:12:02 +0300

    Доделал %d и %i, отредачил все по норме (25 строк)

--- main.c
+++ main.c
@@ -4,21 +4,20 @@
 
 int		main(void)
 {
-	int d  = +3;
-
-//	ft_printf("%06d\n", d);
-//	ft_printf("%+06d\n", d);
-//	ft_printf("% 06d\n", d);
-//	d = +3;
-//	printf("\n");
-//	ft_printf("%06d", d);
-//	ft_printf("%+06d", d);
-//	ft_printf("% 06d", d);
-//	d = +3;
-//	printf("\n");
-	ft_printf("%06d", d);
-	ft_printf("%+06d", d);
-    d = -3;
-	ft_printf("%05d\n", d);
+	int d  = 3;
+	ft_printf("*%+5.8d*\n", d);
+	ft_printf("*%-5.8d*\n", d);
+	ft_printf("*%05.4d*\n", d);
+	ft_printf("*% 5.3d*\n", d);
+	ft_printf("*% 3.5d*\n", d);
+	ft_printf("*% -5d*\n", d);
+	printf("\n");
+	d = -3;
+	ft_printf("*%+5.8d*\n", d);
+	ft_printf("*%-5.8d*\n", d);
+	ft_printf("*%05.4d*\n", d);
+	ft_printf("*% 5.3d*\n", d);
+	ft_printf("*% 3.5d*\n", d);
+	ft_printf("*% -5d*\n", d);
 	return (0);
 }
