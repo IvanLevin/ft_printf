2ee3eb136db70e73bc41dcd91c56b304cf7ccbf6
Breolson <NichkovME@yandex.ru>
2019-02-06 18:00:01 +0300

    Conflicting types errors

--- ft_printf.h
+++ ft_printf.h
@@ -57,10 +57,10 @@ int         d_minus_left(char *arr_1, char *arr_d, size_t len, t_lists *list);
 int         d_minus_right(char *arr_1, char *arr_d, size_t len, t_lists *list);
 int         extra_rigth_d(char *arr_1, char *arr_d, size_t len, t_lists *list);
 void    	ft_printf_x(long long n, t_lists *list);
-int         d_minus_left(char *arr_1, char *arr_d, long long len, t_lists *list);
-int         d_minus_right(char *arr_1, char *arr_d, long long len, t_lists *list);
-int         extra_rigth_d(char *arr_1, char *arr_d, long long len, t_lists *list);
-int         extra_rigth_d_two(char *arr_1, char *arr_d, long long len, t_lists *list);
-char		*ft_itoa_long(long long n);
+//int         d_minus_left(char *arr_1, char *arr_d, long long len, t_lists *list);
+//int         d_minus_right(char *arr_1, char *arr_d, long long len, t_lists *list);
+//int         extra_rigth_d(char *arr_1, char *arr_d, long long len, t_lists *list);
+//int         extra_rigth_d_two(char *arr_1, char *arr_d, long long len, t_lists *list);
+//char		*ft_itoa_long(long long n);
 
 #endif
